d710437bb462e29157a368a1b69190325ec64a8b
c-sivroo <sivannagari.roopa@hidglobal.com>
2019-05-18 15:44:48 +0530

    project name change

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/my-world'),
+      dir: require('path').join(__dirname, '../coverage/my-website'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
